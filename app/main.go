21532e6d8cc5309809f880865ffca421c1ce1361
junnys6018 <junkanglim1@gmail.com>
2022-01-01 18:14:35 +0800

    stage 3: Create a blob object

--- app/main.go
+++ app/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -22,6 +24,7 @@ func main() {
 			fmt.Printf("Error writing file: %s\n", err)
 		}
 		fmt.Println("Initialized git directory")
+
 	case "cat-file":
 		sha := os.Args[3]
 
@@ -41,6 +44,38 @@ func main() {
 				}
 			}
 		}
+
+	case "hash-object":
+		path := os.Args[3]
+
+		if data, err := os.ReadFile(path); err == nil {
+			blob := bytes.Buffer{}
+
+			fmt.Fprintf(&blob, "blob %d", len(data))
+			blob.WriteByte(0)
+			blob.Write(data)
+
+			hash := sha1.Sum(blob.Bytes())
+
+			// Write to disk
+			err := os.Mkdir(fmt.Sprintf(".git/objects/%x/", hash[:1]), 0755)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			compressed := bytes.Buffer{}
+			writer := zlib.NewWriter(&compressed)
+			writer.Write(blob.Bytes())
+			writer.Close()
+
+			err = os.WriteFile(fmt.Sprintf(".git/objects/%x/%x", hash[:1], hash[1:]), compressed.Bytes(), 0644)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Printf("%x", hash)
+		}
+
 	default:
 		fmt.Printf("Unknown command %s\n", command)
 		os.Exit(1)
